1a7e2cce47744d8ee9f5fad1c882dc45c794743a
Vaishali <Prasad@developer.jibe.com>
2015-04-28 17:31:01 +0530

    next log - 2

--- TestGit22/ViewController.m
+++ TestGit22/ViewController.m
@@ -22,6 +22,7 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     NSLog(@"welcome to me");
+    NSLog(@"welcome to 22222");
     // Dispose of any resources that can be recreated.
 }
 
