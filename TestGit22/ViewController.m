fbf758b145321443053ee3fa9955eab6bdeff676
Vaishali <Prasad@developer.jibe.com>
2015-04-29 09:40:46 +0530

    NSLog - welcome 29 - 1

--- TestGit22/ViewController.m
+++ TestGit22/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"welcome - 29 apr");
 }
 
 - (void)didReceiveMemoryWarning {
